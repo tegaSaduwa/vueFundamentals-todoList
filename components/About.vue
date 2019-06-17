bbd54aa8bdc99f452f5d6fa3f98e15a611fd7853
tega <tegasaduwa@yahoo.com>
2019-06-17 14:05:23 +0100

    another commit

---
+++ components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="About">
+    <h1>Vue Practice Todo-list</h1>
+    <p>This is tegas version of brads vue exercise</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+ 
+}
+</script>
+
---
+++ components/TodoItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="todo-item" :class="{ 'is-complete': todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markedComplete">
+      {{ todo.title }}
+      <button  @click="$emit('del-todo', todo.id)" class="del">x</button>
+      
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markedComplete () {
+     this.todo.completed = !this.todo.completed; 
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000; 
+  color: #fff;
+  border:none;
+  border-radius: 100%;
+  padding: 5px 5px;
+  cursor:pointer;
+  float:right;
+}
+
+</style>
+
+
